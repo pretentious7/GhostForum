97be8dcf77725088ea774177b076857564651433
okirpane <31450994+okirpane@users.noreply.github.com>
2020-02-16 02:43:51 -0500

    Add files via upload

--- compiler.js
+++ compiler.js
@@ -1,4 +1,4 @@
-let initString = ['CHADLEY|Hello World!|REPLIES 0|yoooo', 'ABHISHEK|TWO|REPLIES 18|asdfasdf', 'OJ|BOI|REPLIES 24|asdfhc', 'OJJJ|BBBBBBBBB|REPLIES 24|HHHHHHHH']
+let initString = ['HOW|I|THIS|WORKING', 'CHADLEY|Hello World!|REPLIES 0|yoooo', 'ABHISHEK|TWO|REPLIES 18|asdfasdf', 'OJ|BOI|REPLIES 24|asdfhc', 'OJJJ|BBBBBBBBB|REPLIES 24|HHHHHHHH']
 let receiveShit = [];
 let cycles;
 let selfPost = []
@@ -33,32 +33,46 @@ function receiveText() {
 function pushPost() {
     var x = document.createElement("FORM");
     x.setAttribute("id", "posting");
-    x.setAttribute("onsubmit", "return storePost()")
+    x.setAttribute("onsubmit", "return false")
     document.body.appendChild(x);
     var y = document.createElement("INPUT");
     y.setAttribute("type", "text");
-    y.setAttribute("value", "Your name#Your message");
+    y.setAttribute("value", "Your name|Your message");
+    y.setAttribute("id", "userInput");
     document.getElementById("posting").appendChild(y);
     var z = document.createElement("INPUT");
     z.setAttribute("id", "butt")
     z.setAttribute("type", "submit");
+    z.setAttribute("onclick", "storePost()")
     z.setAttribute("value", "Post!");
     document.getElementById("posting").appendChild(z);
-    $('input:text').focus(
+    $('input:text').click(
         function () {
             $(this).val('');
-        });
+    });
+    
 }
 
 function storePost() {
-    console.log('submitted')
     localStorage.setItem('response', 'Hello')
+    var input = document.getElementById('userInput').value;
+    input2 = input + '|REPLIES 0' + '|' + dateCalc()
+    initString.unshift(input2)
+    console.log(input2)
+    console.log(initString)
+    receiveText()
+    document.getElementById("container").innerHTML = "";
+    updateFeed()
+
 }
 
 function updateFeed() {
     let posts = []
     let postContain = []
     let postText = []
+    var container = document.createElement('div')
+    container.id = 'container' 
+    document.body.append(container)
 
     for (i = 0; i < cycles; i++) {
         postText[i] = JSON.parse(localStorage.getItem("pos" + i))
@@ -66,14 +80,14 @@ function updateFeed() {
     for (x = 0; x <= cycles / 4; x++) {
         postContain[i] = document.createElement('div')
         postContain[i].id = "postContainer"+x
+        postContain[i].className = "remove"
         postContain[i].setAttribute("style", "margin-left: 25%; padding: 1em; width: 46%; border: solid; border-radius: 5px ;margin-bottom: 0.25em")
-        document.body.append(postContain[i])
+        document.getElementById("container").append(postContain[i])
         for (i = 4*x; i < 4*x+4; i++) {
             posts[i] = document.createElement('div')
             posts[i].id = "text" + eval(i%4)
             posts[i].innerHTML = postText[i]
             document.getElementById("postContainer"+x).appendChild(posts[i]);   
-            console.log(i%4)
         }
     }
 }
--- script.js
+++ script.js
@@ -1,29 +1,9 @@
 var peer = new Peer({key: 'lwjd5qra8257b9'});
-var db = firebase.firestore();
 let id;
 
 peer.on('open', function(id) {
 	console.log('My peer ID is: ' + id);
-	db.collection("peerjs_ids").doc("id_n").set({
-	id: id 
-});
 	return id
 });
 
-
 console.log('HW');
-
-var docRef = db.collection("peerjs_ids").doc("id_n");
-
-docRef.get().then(function(doc) {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch(function(error) {
-    console.log("Error getting document:", error);
-});
-
-
