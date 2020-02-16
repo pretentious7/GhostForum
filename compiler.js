9c4e4f09545f271001ddbe3e95cb030b5f765f13
okirpane <31450994+okirpane@users.noreply.github.com>
2020-02-16 01:12:27 -0500

    Add files via upload

--- compiler.js
+++ compiler.js
@@ -1,8 +1,12 @@
+let initString = ['CHADLEY|Hello World!|REPLIES 0|yoooo', 'ABHISHEK|TWO|REPLIES 18|asdfasdf', 'OJ|BOI|REPLIES 24|asdfhc', 'OJJJ|BBBBBBBBB|REPLIES 24|HHHHHHHH']
+let receiveShit = [];
+let cycles;
+let selfPost = []
 
 function generateDOM() {
     let divide = []
     let pageTitle = 'CEWITs Funky Forum'
-    let populateText = ['GhostForum', pageTitle, 'Boi']
+    let populateText = ['GhostForum', pageTitle, 'Type your message in the following format:']
 
     for (i = 0; i < 3; i++) {
         divide[i] = document.createElement('div')
@@ -16,28 +20,24 @@ function generateDOM() {
     updateFeed()
 }
 
-let initString = ['CHADLEY|Hello World!|0|yoooo', 'ABHISHEK|TWO|18|asdfasdf', 'OJ|BOI|24|asdfhc']
-let receiveShit = [];
-let cycles;
 function receiveText() {
     for (var i = 0; i < initString.length; i++) {
         receiveShit[i] = initString[i].split("|")
-        for(j=0;j<4;j++){
-            localStorage.setItem('pos'+ eval(4*i+j), JSON.stringify(receiveShit[i][j]))
-            cycles = 4*i+j
+        for (j = 0; j < 4; j++) {
+            localStorage.setItem('pos' + eval(4 * i + j), JSON.stringify(receiveShit[i][j]))
+            cycles = 4 * i + j
         }
     }
-    console.log(receiveShit)
 }
 
 function pushPost() {
     var x = document.createElement("FORM");
     x.setAttribute("id", "posting");
-    x.setAttribute("onsubmit", "storePost()")
+    x.setAttribute("onsubmit", "return storePost()")
     document.body.appendChild(x);
     var y = document.createElement("INPUT");
     y.setAttribute("type", "text");
-    y.setAttribute("value", "Type a message!");
+    y.setAttribute("value", "Your name#Your message");
     document.getElementById("posting").appendChild(y);
     var z = document.createElement("INPUT");
     z.setAttribute("id", "butt")
@@ -50,7 +50,6 @@ function pushPost() {
         });
 }
 
-let selfPost = []
 function storePost() {
     console.log('submitted')
     localStorage.setItem('response', 'Hello')
@@ -58,24 +57,25 @@ function storePost() {
 
 function updateFeed() {
     let posts = []
-    let counter = []
-    let postContain = document.createElement("div")
+    let postContain = []
     let postText = []
-    postContain.id = "postContainer"
-    document.body.append(postContain)
-    for(i=0; i<cycles; i++){
-        postText[i] = JSON.parse(localStorage.getItem("pos"+i)) 
-        counter++
+
+    for (i = 0; i < cycles; i++) {
+        postText[i] = JSON.parse(localStorage.getItem("pos" + i))
     }
-    for(x=0; x<=cycles/4; x++){
-        console.log(x)
-    }
-    for (i = 0; i <= cycles; i++) {
+    for (x = 0; x <= cycles / 4; x++) {
+        postContain[i] = document.createElement('div')
+        postContain[i].id = "postContainer"+x
+        postContain[i].setAttribute("style", "margin-left: 25%; padding: 1em; width: 46%; border: solid; border-radius: 5px ;margin-bottom: 0.25em")
+        document.body.append(postContain[i])
+        for (i = 4*x; i < 4*x+4; i++) {
             posts[i] = document.createElement('div')
-            posts[i].id = "text" + i
+            posts[i].id = "text" + eval(i%4)
             posts[i].innerHTML = postText[i]
-            document.getElementById("postContainer").appendChild(posts[i]);
+            document.getElementById("postContainer"+x).appendChild(posts[i]);   
+            console.log(i%4)
         }
+    }
 }
 
 function dateCalc() {
