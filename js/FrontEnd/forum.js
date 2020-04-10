86650fe302705d4b97cecbf1ddf06ceb66932183
okirpane <31450994+okirpane@users.noreply.github.com>
2020-04-10 16:59:23 -0500

    Add files via upload

---
+++ js/FrontEnd/forum.js
@@ -0,0 +1,32 @@
+// {
+// 	"origin_server_ts": 1526072700313,
+// 	"sender": "@Alice:matrix.alice.tld",
+// 	"event_id": "$1526072700393WQoZb:matrix.alice.tld",
+// 	"unsigned": {
+// 	  "age": 97,
+// 	  "transaction_id": "m1526072700255.17"
+// 	},
+// 	"content": {
+// 	  "body": "Hello Bob and Charlie! Welcome to my room :-)",
+// 	  "msgtype": "m.text"
+// 	},
+// 	"type": "m.room.message",
+// 	"room_id": "!TCnDZIwFBeQyBCciFD:matrix.alice.tld"
+//   }
+
+class Post {
+	constructor(eventID, msgtype, content){
+		this.eventID = eventID;
+		this.msgtype = msgtype;
+		this.content = content
+	}
+}
+
+
+class Forum {
+	constructor(signallingDoc){
+		this.url = signallingDoc.url;
+		this.post 
+		var Forum = new Object();
+	}
+}
