08e02a2ead84b0968260030ae68ef4a2f87d1b35
Abhishek Cherath <abhicherath@gmail.com>
2020-04-12 07:36:07 +0530

    new formatting bois

--- compiler.js
+++ compiler.js
@@ -49,44 +49,18 @@ function generateDOM() {
 function receiveText() {
     for (var i = 0; i < initStringObj.init.length; i++) {
         receiveShit[i] = initStringObj.init[i].split("|")
-        for (j = 0; j < 4; j++) {
+        for (j = 0; j < 3; j++) {
             //localStorage.setItem('pos' + eval(4 * i + j), JSON.stringify(receiveShit[i][j]))
-            localStorage.setItem('pos' + eval(4 * i + j), receiveShit[i][j])
-            cycles = 4 * i + j
+            localStorage.setItem('pos' + eval(3 * i + j), receiveShit[i][j])
+            cycles = 3 * i + j
         }
     }
-	//cycles = 4*initString.length+3;
+    //cycles = 4*initString.length+3;
 }
 
 
 
 function pushPost() {
-    var x = document.createElement("FORM");
-    x.setAttribute("id", "posting");
-    x.setAttribute("onsubmit", "return false")
-    document.body.appendChild(x);
-    var y = document.createElement("INPUT");
-    y.setAttribute("type", "text");
-    y.setAttribute("value", "Your name|Your message");
-    y.setAttribute("id", "userInput");
-    document.getElementById("posting").appendChild(y);
-    var z = document.createElement("INPUT");
-    z.setAttribute("id", "butt")
-    z.setAttribute("type", "submit");
-    z.setAttribute("onclick", "storePost(); sendMessage();") //communicator sends. so peer.send here.
-    z.setAttribute("value", "Post!");
-    document.getElementById("posting").appendChild(z);
-    $('input:text').click(
-        function () {
-            $(this).val('');
-    });
-    if (typeof(conn) !== 'undefined'){
-	conn.send(initString);
-}
-
-}
-
-function storePost() {
 
     var a = document.createElement("FORM")
     a.setAttribute("id", "username")
@@ -99,21 +73,67 @@ function storePost() {
     b.setAttribute("id", "usernameInput");
     document.body.appendChild(b)
 
-    b.setAttribute("style", "margin-left: 25%; padding: 1em; width: 30%; margin-bottom: 1em; border:none; border-bottom: 1px solid black; background-color: #FFFDF5; height: 13px");
+    b.setAttribute("style", "margin-left: 25%; padding: 1em; width: 30%; margin-bottom: 1em; border:none; border-bottom: 1px solid black; background-color: #FFFDF5; height: 13px")
 
+    var x = document.createElement("FORM");
+    x.setAttribute("id", "posting");
+    x.setAttribute("onsubmit", "return false")
+    document.body.appendChild(x);
+    var y = document.createElement("INPUT");
+    y.setAttribute("type", "text");
+    y.setAttribute("value", "Your message");
+    y.setAttribute("id", "userInput");
+    document.getElementById("posting").appendChild(y);
+    var z = document.createElement("INPUT");
+    z.setAttribute("id", "butt")
+    z.setAttribute("type", "submit");
+    z.setAttribute("onclick", "storePost(); sendMessage();") //communicator sends. so peer.send here.
+    z.setAttribute("value", "Post!");
+    document.getElementById("posting").appendChild(z);
+    $('input:text').click(
+        function () {
+            $(this).val('');
+        });
+    if (typeof (conn) !== 'undefined') {
+        conn.send(initString);
+    }
+}
+function postConstruct(u, i){
+	let transID = Math.floor(Math.random()*16777215).toString(16);
+	let roomID = Math.floor(Math.random()*16777215).toString(16);
+	var post = {
+		origin_server_ts: '',
+		sender: '@' + u + ':ghostforum.tech',
+		unsigned: {
+			age: 'x',
+			transaction_id: transID
+		},
+		content: {
+			body: i,
+			msgtype: 'm.text'
+		},
+		type: 'm.room.message',
+		room_id: 'communism'+roomID //put page title (from url) here
+	}
+    postObj = JSON.stringify(post);
+    //console.log(postObj)
+}
+
+function storePost() {
     localStorage.setItem('response', 'Hello')
+    var un = document.getElementById('usernameInput').value
     var input = document.getElementById('userInput').value;
-    input2 = input + '|REPLIES 0' + '|' + dateCalc()
+    input2 = un + '|' + input + '|' + dateCalc()
     initStringObj.init.unshift(input2)
     console.log(input2)
     console.log(initString)
     receiveText()
     document.getElementById("container").innerHTML = "";
     updateFeed()
-
+    //postConstruct(un,input)
 }
 
-function arbitraryStorePost(){
+function arbitraryStorePost() {
     receiveText()
     document.getElementById("container").innerHTML = "";
     updateFeed()
@@ -124,28 +144,28 @@ function updateFeed() {
     let postContain = []
     let postText = []
     var container = document.createElement('div')
-    container.id = 'container' 
+    container.id = 'container'
     document.body.append(container)
 
     for (i = 0; i < cycles; i++) {
         //postText[i] = JSON.parse(localStorage.getItem("pos" + i))
         postText[i] = localStorage.getItem("pos" + i)
     }
-    for (x = 0; x <= cycles / 4; x++) {
+    for (x = 0; x <= cycles / 3; x++) {
         postContain[i] = document.createElement('div')
-        postContain[i].id = "postContainer"+x
+        postContain[i].id = "postContainer" + x
         postContain[i].className = "remove"
         postContain[i].setAttribute("style", "margin-left: 25%; padding: 1em; width: 46%; border: solid; border-radius: 5px ;margin-bottom: 0.25em")
         document.getElementById("container").append(postContain[i])
-        for (i = 4*x; i < 4*x+4; i++) {
+        for (i = 3 * x; i < 3 * x + 3; i++) {
             posts[i] = document.createElement('div')
-            posts[i].id = "text" + eval(i%4)
+            posts[i].id = "text" + eval(i % 3)
             posts[i].innerHTML = postText[i]
-            document.getElementById("postContainer"+x).appendChild(posts[i]);   
+            document.getElementById("postContainer" + x).appendChild(posts[i]);
         }
     }
-	//stores for future instance in localstorage everythin
-	localStorage.setItem('initString'+fname, initStringObj.init);
+    //stores for future instance in localstorage everythin
+    localStorage.setItem('initString'+fname, initStringObj.init);
 }
 
 function dateCalc() {
@@ -157,15 +177,14 @@ function dateCalc() {
     return dateString;
 }
 
-function sendMessage(){
-	if(typeof conner !== 'undefined'){
-		conner.send(initStringObj.init);
-		console.log(conner);
-	}
-	if(typeof connection !== 'undefined'){
-		connection.send(initStringObj.init);
-	}
-	
+function sendMessage() {
+    if (typeof conner !== 'undefined') {
+        conner.send(initStringObj.init);
+    }
+    if (typeof connection !== 'undefined') {
+        connection.send(initStringObj.init);
+    }
+
 }
-	
+
 
